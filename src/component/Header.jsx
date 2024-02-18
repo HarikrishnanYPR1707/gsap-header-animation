cad117c3688d8a39ed0ed55e1a5cbf538b66d1b2
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-18 12:31:52 +0530

    Added Changes

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="uppercase font-bold font-spaceGrotesk">
-      {"Harikrishnan".split("").map((item, index) => (
-        <span className="border border-purple-500">{item}</span>
+    <div className="text-9xl h-[140px] border w-fit bg-clip-text text-transparent bg-gradient-to-tr from-blue-700 to-green-500 font-bold font-spaceGrotesk">
+      {"Amazing".split("").map((item, index) => (
+        <span className="">{item}</span>
       ))}
     </div>
   );
