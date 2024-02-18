d69a6171246448955c5e4c933f3d866ce02df8d1
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-18 12:23:18 +0530

    Added component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./component/Header";
 
 const App = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="bg-gray-950 h-screen border text-white">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
