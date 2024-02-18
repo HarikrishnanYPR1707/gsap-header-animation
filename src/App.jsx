4d8cfaef4514652545c063501eaf1f7b5ca3001d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-18 12:31:47 +0530

    Center the content

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./component/Header";
 
 const App = () => {
   return (
-    <div className="bg-gray-950 h-screen border text-white">
+    <div className="bg-gray-950 h-screen border flex justify-center text-white">
       <Header />
     </div>
   );
