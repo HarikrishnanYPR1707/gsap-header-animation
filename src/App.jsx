ebc14a91cf152e9426adbcef67a9f1516e7c5774
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-18 12:59:31 +0530

    Added Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-import Header from "./component/Header";
+import React, { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 
 const App = () => {
+  const comp = useRef(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline();
+
+      t1.from("#character", {
+        xPercent: "100",
+        // rotate: 90,
+        // delay: 0.3,
+        stagger: 0.5,
+      });
+    }, comp);
+
+    return () => ctx.revert();
+  }, []);
   return (
-    <div className="bg-gray-950 h-screen border flex justify-center text-white">
-      <Header />
+    <div
+      ref={comp}
+      className="bg-gray-950 relative h-screen border flex justify-center text-white"
+    >
+      <div
+        id="main-container"
+        className="text-9xl h-[140px] absolute mt-20 border border-red-500 w-fit bg-clip-text text-transparent bg-gradient-to-tr from-blue-700 to-green-500 font-bold font-spaceGrotesk"
+      >
+        {"Amazing".split("").map((item, index) => (
+          <span id="character" key={index} className="border">
+            {item}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
