a31b4d0487723dd79d2e9430196c911df1b109c1
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-19 10:35:05 +0530

    Done with Header Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 
 const App = () => {
   const comp = useRef(null);
 
   useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      const t1 = gsap.timeline();
+    const ctx = gsap.context(() => {
+      const timeline = gsap.timeline();
 
-      t1.from("#character", {
-        xPercent: "100",
-        // rotate: 90,
-        // delay: 0.3,
-        stagger: 0.5,
-      });
+      // .from("#main-container", {
+      //   top: "50%",
+      //   opacity: 0,
+      //   duration: 1,
+      // })
+
+      timeline
+        // .from("#main-container", {
+        //   top: "50%",
+        //   opacity: 0,
+        //   duration: 1,
+        // })
+        .from("#character", {
+          top: "150%",
+          duration: 1,
+          stagger: 0.19,
+        });
     }, comp);
 
     return () => ctx.revert();
   }, []);
+
   return (
     <div
       ref={comp}
-      className="bg-gray-950 relative h-screen border flex justify-center text-white"
+      className="bg-gray-950 relative h-screen flex justify-center text-white"
     >
       <div
         id="main-container"
-        className="text-9xl h-[140px] absolute mt-20 border border-red-500 w-fit bg-clip-text text-transparent bg-gradient-to-tr from-blue-700 to-green-500 font-bold font-spaceGrotesk"
+        className="text-9xl h-[140px] mt-20 relative font-rubikMonoOne w-[761px] overflow-hidden [&>*:nth-child(1)]:left-0 [&>*:nth-child(2)]:left-[109px] [&>*:nth-child(3)]:left-[218px] [&>*:nth-child(4)]:left-[326px] [&>*:nth-child(5)]:left-[435px] [&>*:nth-child(6)]:left-[544px] [&>*:nth-child(7)]:left-[653px]"
       >
-        {"Amazing".split("").map((item, index) => (
-          <span id="character" key={index} className="border">
-            {item}
-          </span>
-        ))}
+        {"Amazing".split("").map((item, index) => {
+          return (
+            <span id="character" key={index} className="absolute">
+              {item}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
